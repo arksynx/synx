48e3bcba082a707ac84a2d9926fd4ed19875cb49
ark <jaylua2548@gmail.com>
2025-08-23 21:42:08 +0800

    Update library.lua

--- library.lua
+++ library.lua
@@ -294,7 +294,7 @@ do
 
 		local toggleButton = Instance.new("TextButton")
 toggleButton.Name = "ToggleButton"
-toggleButton.Text = "Show Library"
+toggleButton.Text = "ALLOSTRA"
 toggleButton.BackgroundTransparency = 0.3
 toggleButton.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
 toggleButton.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -321,9 +321,9 @@ local function toggleUI()
     container.Main.Visible = uiVisible
 
     if uiVisible then
-        toggleButton.Text = "Hide Library"
+        toggleButton.Text = "Hide ALLOSTRA"
     else
-        toggleButton.Text = "Show Library"
+        toggleButton.Text = "Show ALLOSTRA"
     end
 end
 
